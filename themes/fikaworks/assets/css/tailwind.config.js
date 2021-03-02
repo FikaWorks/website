5f797e6dbf1da12d26aff42eaab94ea49d0a6ba1
Etienne Tremel <etienne@kogusenn.com>
2021-03-02 18:04:33 +0100

    adjust FikaWorks layout with tailwind

--- themes/fikaworks/assets/css/tailwind.config.js
+++ themes/fikaworks/assets/css/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-    theme: {
-      extend: {}
-    },
-    variants: {},
-    plugins: []
-  }
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#0d2a72'
+        },
+        secondary: {
+          DEFAULT: '#a01077'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+  // purge: {
+  //   enabled: true,
+  //   content: [
+  //     './**/*.html'
+  //   ]
+  // }
+}
