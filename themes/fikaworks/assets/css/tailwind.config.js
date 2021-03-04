6d91bc72cadc3495e39fbbf8dde2530b628515d1
Etienne Tremel <etienne@kogusenn.com>
2021-03-04 20:14:04 +0100

    few more layout change

--- themes/fikaworks/assets/css/tailwind.config.js
+++ themes/fikaworks/assets/css/tailwind.config.js
@@ -2,17 +2,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
+        blue: {
           DEFAULT: '#0d2a72'
         },
-        secondary: {
+        purple: {
           DEFAULT: '#a01077'
-        }
+        },
+        aqua: {
+          DEFAULT: '#54b5b5'
+        },
+        yellow: {
+          700: '#cc830e',
+          DEFAULT: '#ffc600'
+        },
       }
     }
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
   // purge: {
   //   enabled: true,
   //   content: [
