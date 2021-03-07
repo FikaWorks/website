934f6521f8be03f73af35d9e6bdcb10e44b92f31
Etienne Tremel <etienne@kogusenn.com>
2021-03-07 18:39:34 +0100

    change hero banner and remove rounded border from post section

--- themes/fikaworks/assets/css/tailwind.config.js
+++ themes/fikaworks/assets/css/tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
           DEFAULT: '#0d2a72'
         },
         purple: {
+          300: '#b61c8a',
+          500: '#8c0d68',
           DEFAULT: '#a01077'
         },
         aqua: {
+          300: '#6dcece',
+          500: '#49b0b0',
           DEFAULT: '#54b5b5'
         },
         yellow: {
@@ -23,10 +27,10 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
-  // purge: {
-  //   enabled: true,
-  //   content: [
-  //     './**/*.html'
-  //   ]
-  // }
+  purge: {
+    enabled: true,
+    content: [
+      './**/*.html'
+    ]
+  }
 }
