ad1501c644b14d3a5523e640b9b179ad4dbd0050
Etienne Tremel <etienne@kogusenn.com>
2021-03-10 21:39:39 +0100

    add fade-in transition on scroll

---
+++ themes/fikaworks/assets/js/main.js
@@ -0,0 +1,23 @@
+require('intersection-observer');
+
+// add transition css classes for slow fade in when the element appear in the
+// viewport
+window.onload = () => {
+  const callback = function(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("motion-safe:animate-fadeIn");
+      } else {
+        entry.target.classList.remove("motion-safe:animate-fadeIn");
+      }
+    });
+  };
+
+  const targets = document.querySelectorAll(".show-on-scroll");
+  const observer = new IntersectionObserver(callback);
+
+  targets.forEach(function(target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+  });
+};
